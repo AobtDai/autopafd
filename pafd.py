f49ac1b819b1f0290a0c358e4b229682cfee73a9
aoweiyin <2570568646@qq.com>
2021-07-26 11:05:43 +0800

    beta 2.0（想办法解决网站上位置信息无法获取的问题

--- pafd.py
+++ pafd.py
@@ -4,10 +4,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import time
 
 chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--disable-gpu')
 browser = webdriver.Chrome(options=chrome_options)
 WAIT = WebDriverWait(browser, 10)
 browser.set_window_size(1400,900)
@@ -17,6 +18,14 @@ if __name__ == '__main__':
     # url = "https://uis.fudan.edu.cn/"
     browser.get(url)
 
+    # window = browser.find_element(By.XPATH, '//*[@class="modal_close"]')
+    # if len(window)!=0:
+    #     goon = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="modal_close"]')))
+    #     goon.click()
+
+    # time.sleep(3)
+    # browser.refresh()
+
     username = WAIT.until(EC.presence_of_element_located(((By.XPATH, '//*[@id="username"]'))))
     username.send_keys('')
     passwd = WAIT.until(EC.presence_of_element_located(((By.XPATH, '//*[@id="password"]'))))
@@ -24,7 +33,9 @@ if __name__ == '__main__':
     signin = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="idcheckloginbtn"]')))
     signin.click()
 
-    submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="tab-title-desc-little"]')))
+    confirm = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="wapat-btn wapat-btn-ok"]')))
+    confirm.click()
+    submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="footers"]/a')))
     submit.click()
 
 print("Perfect signin")
